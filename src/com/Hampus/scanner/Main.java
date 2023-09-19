29a28d80979c743d4a411f6c36b87d7a5143530a
HampusCastle <Hampuscastle@gmail.com>
2023-09-19 13:14:27 +0200

    Scanner practice

--- src/com/Hampus/scanner/Main.java
+++ src/com/Hampus/scanner/Main.java
@@ -1,8 +1,51 @@
 package com.Hampus.scanner;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+    boolean isPlaying = true;
+
+        do {
+            int result = scannerNumber();
+
+            if (result > 1000) {
+                isPlaying = false;
+                break;
+            }
+
+
+            for(int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping..");
+
+            }
+
+
+
+
+
+        } while(isPlaying);
+
     }
-}
+
+    public static int scannerNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+
+
+        return sc.nextInt();
+    }
+    public static String scannerString() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert a sentence please");
+        String input = sc.nextLine();
+
+        return input;
+    }
+    }
+
